10af28a6f2304090e1528e2a27ee9531b8cd25ab
reachlinlen <reachlinlen@gmail.com>
2019-06-11 08:03:36 +0800

    Line Chart Reworked

--- src/LineChart/index.js
+++ src/LineChart/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-const SVG_WTH = 720, SVG_HT = 410, LEG_COLORS = ["red", "green", "blue"];
+const SVG_WTH = 720, SVG_HT = 403, LEG_COLORS = ["red", "green", "blue"];
 var d3 = require("d3");
 export default function LineChart(props) {
   useEffect(() => {
@@ -8,16 +8,16 @@ export default function LineChart(props) {
     let data = [
       ["transID01", "USER - A", "American Express", 2500],
       ["transID02", "USER - B", "American Express", 2500],
-      ["transID03", "USER - C", "American Express", 1500],
-      ["transID13", "USER - C", "American Express",  500],
+      ["transID03", "USER - C", "American Express", 1750],
+      ["transID13", "USER - C", "American Express",  750],
       ["transID04", "USER - A", "DBS", 2500],
       ["transID05", "USER - B", "DBS", 1500],
       ["transID06", "USER - C", "DBS", 500],
       ["transID07", "USER - A", "VISA", 2500],
-      ["transID07", "USER - B", "VISA", 1500],
-      ["transID07", "USER - C", "VISA", 500],
+      ["transID07", "USER - B", "VISA", 1250],
+      ["transID07", "USER - C", "VISA", 250],
     ];
-    let finalData = {}, cards = [], maxAmt = 0, maxTran = 0;
+    let finalData = {}, cards = [], maxAmt = 0, maxTran = 0, txnNos = new Set();
     data.forEach(d => {
       let ind = cards.indexOf(d[2]);
       maxAmt = Math.max(maxAmt, d[3]);
@@ -31,6 +31,7 @@ export default function LineChart(props) {
     });
     Object.entries(finalData).forEach(([key, value]) => {
       Object.entries(value).forEach(([key, val]) => {
+        txnNos.add(val);
         maxTran = Math.max(maxTran, val);
       });
     });
@@ -38,32 +39,68 @@ export default function LineChart(props) {
                   .append("g")
                   .attr("id", "tran-line-chart");
         // svg.selectAll
-        svg.selectAll("legend-rect")
-                  .data(cards)
-                  .enter().append("rect")
-                  .attr("x", (d,i) => (200 + i*100))
-                  .attr("y", 10)
-                  .attr("width", 1.5+"vw")
-                  .attr("height", 1.5+"vh")
-                  .attr("fill", (d,i) => LEG_COLORS[i]);
+    svg.selectAll("legend-rect")
+              .data(cards)
+              .enter().append("rect")
+              .attr("x", (d,i) => (200 + i*100))
+              .attr("y", 10)
+              .attr("width", 1.5+"vw")
+              .attr("height", 1.5+"vh")
+              .attr("fill", (d,i) => LEG_COLORS[i]);
+    cards.forEach((card, index) => {
+      if (card.length > 8) {
         svg.selectAll("legend-text")
-                  .data(cards)
-                  .enter().append("text")
-                  .text(d => d)
-                  .attr("x", (d,i) => (230 + i*100))
-                  .attr("y", 20)
-                  .attr("width", 1.5+"vw")
-                  .attr("height", 1.5+"vh")
-                  .attr("stroke", "#000");
-    let scaleLeft = d3.scaleLinear().domain([maxAmt, 0]).range([0,0.8*SVG_HT]);
+            .data([card])
+            .enter().append("text")
+            .text(d => d.slice(0,9))
+            .attr("x", d => (230 + index*100))
+            .attr("y", 20)
+            .attr("width", 1.5+"vw")
+            .attr("height", 1.5+"vh")
+            .attr("stroke", "#000");
+        svg.selectAll("legend-text")
+            .data([card])
+            .enter().append("text")
+            .text(d => d.slice(9))
+            .attr("x", d => (230 + index*100))
+            .attr("y", 40)
+            .attr("width", 1.5+"vw")
+            .attr("height", 1.5+"vh")
+            .attr("stroke", "#000");
+      }
+      else {
+        svg.selectAll("legend-text")
+            .data([card])
+            .enter().append("text")
+            .text(d => d)
+            .attr("x", (d,i) => (230 + index*100))
+            .attr("y", 20)
+            .attr("width", 1.5+"vw")
+            .attr("height", 1.5+"vh")
+            .attr("stroke", "#000");
+      }
+    });
+    let scaleLeft = d3.scaleLinear().domain([0, maxAmt]).range([0.8*SVG_HT,0]);
     let leftAxis = d3.axisLeft(scaleLeft).ticks(8);
         svg.append("g")
             .attr("transform", "translate(80,50)")
             .call(leftAxis);
     let scaleBot = d3.scaleLinear().domain([0, maxTran]).range([0,0.8*SVG_WTH]);
-    let botAxis = d3.axisBottom(scaleBot).ticks(8);
+    let botAxis;
+    if (txnNos.size > 8) botAxis = d3.axisBottom(scaleBot).ticks(8);
+    else {
+      let tickVal = [];
+      let iterator = txnNos.values();
+      let val = iterator.next().value;
+      while (val !== undefined) {
+        tickVal.push(val);
+        val = iterator.next().value;
+      }
+      botAxis = d3.axisBottom(scaleBot).tickValues(tickVal);
+    }
+    // let botAxis = d3.axisBottom(scaleBot).ticks(8);
         svg.append("g")
-            .attr("transform", "translate(80, 370)")
+            .attr("transform", "translate(80, 373)")
             .call(botAxis);
     let points = [];
     Object.entries(finalData).forEach(([key, value]) => {
@@ -75,31 +112,40 @@ export default function LineChart(props) {
       });
       points.push(axisPts);
     });
+    let a;
+    points.forEach(p => {
+      for(let i=0;i < p.length-1;i++) {
+        for(let j=i+1;j < p.length;j++) {
+          if (p[i]['x'] > p[j]['x']) {
+            a = p[i];
+            p[i] = p[j];
+            p[j] = a;
+          }
+          if (p[i]['x'] === p[j]['x'] && p[i]['y'] < p[j]['y']) {
+            a = p[i];
+            p[i] = p[j];
+            p[j] = a;
+          }
+        }
+      }
+    });
     let lineFunc = d3.line()
-                      .x(d => d.x)
-                      .y(d => d.y)
+                      .x(d => d.x+80)
+                      .y(d => d.y+50)
                       .curve(d3.curveMonotoneX);
-    svg.append("path")
-        .attr("d", lineFunc(points[0]))
-        .attr("stroke", "blue")
+    points.forEach((p,ind) => {
+      svg.append("path")
+        .attr("class","line-"+ind)
+        .attr("d", lineFunc(p))
+        .attr("stroke", LEG_COLORS[ind])
         .attr("stroke-width", 2)
         .attr("fill", "none");
-    // points.forEach(p => {
-    //   for(let i=0;i < (p.length-2);i++) {
-    //     for(let j=i;j < (p.length-1);j++) {
-    //       if (p[j][0] > p[j+1][0]) {
-    //         let a = p[j][0];
-    //         p[j][0] = p[j+1][0];
-    //         p[j+1][0] = a;
-    //       }
-    //     }
-    //   }
-    // });
+    })
   });
 
   return (
-    <div className="svg-line" style={{margin: '2vh 0 0 15vw', height: '50vh', width: '50vw', backgroundColor: '#b6b9ba'}}>
-      <svg width={'100%'} height={'100%'} id={"line-chart"} />
+    <div className="svg-line" style={{height: SVG_HT, width: SVG_WTH, backgroundColor: '#b6b9ba'}}>
+      <svg width={SVG_WTH} height={SVG_HT} id={"line-chart"} />
     </div>
   );
 }
