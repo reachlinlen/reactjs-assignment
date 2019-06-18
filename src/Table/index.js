04c99dfd3ae611389bbb03be215ddd51002e0a09
Lenin M <reachlinlen@gmail.com>
2019-06-18 22:47:17 +0800

    Improved the code of Table component

--- src/Table/index.js
+++ src/Table/index.js
@@ -4,21 +4,19 @@ import Row from './Row';
 import { connect } from 'react-redux';
 import Button from '@material-ui/core/Button';
 
-// const NO_ROWS = 5;
 const NOROWS_PAGE = 1;
 function Table(props) {
   const [rowElements, setRowElements] = useState([]);
   const [pageNo, setPageNo] = useState(1);
   const [pageButtons, setPageButtons] = useState([]); 
-  // [a,b] => a is list of pages ['>'&'<' in string; other pages in nos]
-  //       => b is pageButtons in JSX format
-  let totPages = 1, currPages = [];
+  // [a,b,t] => a is list of pages ['>'&'<' in string; other pages in nos]
+  //         => b is pageButtons in JSX format
+  //         => t is totpages
 
   useEffect(() => {
     let txnData = props.txnData, latestTxns = [];
     if (txnData.length > NOROWS_PAGE) {
       if (pageNo === 1) latestTxns = txnData.slice(0,NOROWS_PAGE);
-      // else latestTxns = txnData.slice(pageNo*NOROWS_PAGE, (pageNo*NOROWS_PAGE)+NOROWS_PAGE);
       else latestTxns = txnData.slice((pageNo-1)*NOROWS_PAGE).slice(0,NOROWS_PAGE);
     } else latestTxns = txnData;
     let allRows = latestTxns.map(r => {
@@ -29,7 +27,7 @@ function Table(props) {
 
   useEffect(() => {
   let currTotPages = Math.trunc(props.txnData.length/NOROWS_PAGE) + 
-                        (props.txnData.length%NOROWS_PAGE !== 0 ? 1 : 0);
+                               (props.txnData.length%NOROWS_PAGE !== 0 ? 1 : 0);
   let locPageBut, prevTotPages;
   if (pageButtons.length > 0) {
     locPageBut = [...pageButtons[0]];
@@ -42,35 +40,31 @@ function Table(props) {
     locPageBut = [];
     prevTotPages = 0;
   }
-  // if (currTotPages < 7 && prevTotPages !== currTotPages && currTotPages > 1) {
   if (prevTotPages !== currTotPages && currTotPages > 1) {
     switch (currTotPages) {
       case 2:
-        createPageBut([1,2]);
+        createPageBut([1,2], currTotPages);
         break;
       case 3:
-        createPageBut([1,2,3]);
+        createPageBut([1,2,3], currTotPages);
         break
       case 4:
-        createPageBut([1,2,3,4]);
+        createPageBut([1,2,3,4], currTotPages);
         break;
       case 5:
-        createPageBut([1,2,3,4,5]);
+        createPageBut([1,2,3,4,5], currTotPages);
         break;
       case 6:
-        createPageBut([1,2,3,4,5,'>']);
+        createPageBut([1,2,3,4,5,'>'], currTotPages);
         break;
       default:
-        createPageBut(pageButtons[0]);
+        createPageBut(pageButtons[0], currTotPages);
         break;
-      // case 7:
-      //   createPageBut([2,3,4,5,6,'>']);
-      //   break;
     }
    }  
   }, [props.txnData]);
 
-  const createPageBut = (pages) => {
+  const createPageBut = (pages, currTotPages) => {
     let newPageButtons = 
                 pages.map(e => {
                   if (e === '>' || e === '<') {
@@ -82,7 +76,7 @@ function Table(props) {
                 });
     // if (pages[pages.length-1] === '>') pages.pop();
     // if (pages[0] === '<') pages.shift();
-    setPageButtons([pages, newPageButtons]);
+    setPageButtons([pages, newPageButtons, currTotPages]);
   }
 
   const handlePageBtnClick = (e) => {
@@ -90,9 +84,7 @@ function Table(props) {
   }
 
   const handleArrowBtnClick = (e) => {
-    let totPages = Math.trunc(props.txnData.length/NOROWS_PAGE) + 
-                              (props.txnData.length%NOROWS_PAGE !== 0 ? 1 : 0),
-        len = pageButtons[0].length, listPages = [];
+    let totPages = pageButtons[2], len = pageButtons[0].length, listPages = [];
     if (pageButtons[0][len-1] === '>') {pageButtons[0].pop(); --len;}
     if (pageButtons[0][0] === '<') {pageButtons[0].shift(); --len;}
     if (e.target.innerText === '<') 
